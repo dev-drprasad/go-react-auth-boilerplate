62acd8b48d281cff6c8bd523cfe2c941ff01aca1
REDDY PRASAD <dev.drprasad@aim.com>
2020-11-10 22:49:28 +0530

    🎨 Add layout

--- web/src/Main.js
+++ web/src/Main.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Menu } from "antd";
 
-const { Content } = Layout;
+const { Content, Sider } = Layout;
 
 function Main({ children }) {
   return (
-    <Layout>
-      <Layout className="site-layout">
+    <Layout className="site-layout" style={{ height: "100%" }}>
+      <Sider width={200}>
+        <Menu mode="inline" style={{ height: "100%" }}>
+          <Menu.Item key="1">option1</Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
         <Content style={{ padding: "32px 16px 16px 16px" }} id="main">
           {children}
         </Content>
